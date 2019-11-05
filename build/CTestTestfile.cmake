eda714862dc75c6ce937b258572872c284e5ef6d
John MacDonald <johnmccambridge7@gmail.com>
2019-11-05 03:16:49 +0000

    project setup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/husarion/final_project/src
+# Build directory: /home/husarion/final_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(final_project)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/husarion/final_project/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/husarion/final_project/build"
+export ROS_PACKAGE_PATH="/home/husarion/final_project/src:$ROS_PACKAGE_PATH"
