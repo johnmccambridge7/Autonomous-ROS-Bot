18cf7eeaf3c96d99bec9a5fa29e9948762575540
John MacDonald <johnmccambridge7@gmail.com>
2019-11-19 05:14:29 +0000

    path planning

--- src/final_project/src/TestImage.py
+++ src/final_project/src/TestImage.py
@@ -11,8 +11,223 @@ import numpy as np
 import math
 from collections import deque
 
-class ImageCapture:
-    def __init__(self):
+# Start and end nodes, and starting directions
+START = 0
+END = 15
+DIRECTION = 0
+
+# -----------------------------------------------------------------------
+# START OF BFS PATH PLANNING
+
+# Node class for intersections
+class Node:
+	def __init__(self, name):
+		self.name = name
+		self.previous = None
+		self.up = None
+		self.right = None
+		self.down = None
+		self.left = None
+
+
+# Making the map
+def make_map():
+	# Make all the nodes first
+	node_list = []
+	node_list.append(Node("A"))
+	node_list.append(Node("B"))
+	node_list.append(Node("C"))
+	node_list.append(Node("D"))
+	node_list.append(Node("E"))
+	node_list.append(Node("F"))
+	node_list.append(Node("G"))
+	node_list.append(Node("H"))
+	node_list.append(Node("I"))
+	node_list.append(Node("J"))
+	node_list.append(Node("K"))
+	node_list.append(Node("L"))
+	node_list.append(Node("M"))
+	node_list.append(Node("N"))
+	node_list.append(Node("O"))
+	node_list.append(Node("P"))
+
+	# Make the connections between nodes
+	node_list[0].right = Node_List[1]
+	node_list[1].right = Node_List[2]
+	node_list[2].right = Node_List[3]
+	node_list[4].right = Node_List[5]
+	node_list[5].right = Node_List[6]
+	node_list[7].right = Node_List[8]
+	node_list[8].right = Node_List[9]
+	node_list[9].right = Node_List[10]
+	node_list[10].right = Node_List[11]
+	node_list[12].right = Node_List[13]
+	node_list[13].right = Node_List[14]
+	node_list[14].right = Node_List[15]
+
+	node_list[1].left = Node_List[0]
+	node_list[2].left = Node_List[1]
+	node_list[3].left = Node_List[2]
+	node_list[5].left = Node_List[4]
+	node_list[6].left = Node_List[5]
+	node_list[8].left = Node_List[7]
+	node_list[9].left = Node_List[8]
+	node_list[10].left = Node_List[9]
+	node_list[11].left = Node_List[10]
+	node_list[13].left = Node_List[12]
+	node_list[14].left = Node_List[13]
+	node_list[15].left = Node_List[14]
+
+	node_list[4].up = Node_List[0]
+	node_list[5].up = Node_List[1]
+	node_list[7].up = Node_List[4]
+	node_list[8].up = Node_List[5]
+	node_list[9].up = Node_List[6]
+	node_list[10].up = Node_List[2]
+	node_list[11].up = Node_List[3]
+	node_list[12].up = Node_List[7]
+	node_list[13].up = Node_List[8]
+	node_list[14].up = Node_List[9]
+	node_list[15].up = Node_List[11]
+
+	node_list[0].down = Node_List[4]
+	node_list[1].down = Node_List[5]
+	node_list[2].down = Node_List[10]
+	node_list[3].down = Node_List[11]
+	node_list[4].down = Node_List[7]
+	node_list[5].down = Node_List[8]
+	node_list[6].down = Node_List[9]
+	node_list[7].down = Node_List[12]
+	node_list[8].down = Node_List[13]
+	node_list[9].down = Node_List[14]
+	node_list[11].down = Node_List[15]
+
+	return node_list
+
+
+# Expanding a node
+def expand(node, frontier, seen):
+	next_node = node.up
+	if next_node != None and next_node not in seen:
+		frontier.append(next_node)
+		seen.append(next_node)
+		next_node.previous = node
+
+	next_node = node.right
+	if next_node != None and next_node not in seen:
+		frontier.append(next_node)
+		seen.append(next_node)
+		next_node.previous = node
+
+	next_node = node.down
+	if next_node != None and next_node not in seen:
+		frontier.append(next_node)
+		seen.append(next_node)
+		next_node.previous = node
+
+	next_node = node.left
+	if next_node != None and next_node not in seen:
+		frontier.append(next_node)
+		seen.append(next_node)
+ 		next_node.previous = node
+
+# Convert Global direction to local robot turns
+def convert_direction(result, robot_direction):
+	# Convert to local direction
+	for i in range(len(result)):
+		temp = result[i]
+		result[i] = (result[i] - robot_direction) % 4
+		robot_direction = temp
+
+	# Convert directions to be consitent with robot directions
+	# 0 = straight, 1 = left, -1 = right
+	for i in range(len(result)):
+		if result[i] == 1:
+			result[i] = -1
+
+		if result[i] == 3:
+			result[i] = 1
+
+	# Make the list into a deque
+	final = deque()
+	for i in range(len(result)):
+		final.append(result[i])
+
+	return final
+
+
+# What to do if solved
+def solved(node, robot_direction):
+	reorder = []
+
+	while node is not None:
+		reorder.insert(0, node)
+		node = node.previous
+
+	# We now have the path from node to node
+	# Now, convert this to left and right turns
+
+	result = []
+	for i in range(len(reorder)):
+		if i != len(reorder)-1:
+ 			if reorder[i].up == reorder[i + 1]:
+				result.append(0)
+
+			if reorder[i].right == reorder[i + 1]:
+				result.append(1)
+
+			if reorder[i].down == reorder[i + 1]:
+				result.append(2)
+
+			if reorder[i].left == reorder[i + 1]:
+				result.append(3)
+
+	return convert_direction(result, robot_Direction)
+
+
+# Check if it is Goal
+def is_goal(end, node):
+	if end.name == node.name:
+		return True
+	return False
+
+
+# Search algorithm
+def bfs(start, end, direction):
+	# Initialize some variables
+	frontier = []
+	seen = []
+	frontier.append(start)
+	seen.append(start)
+
+	# Infinite loop, main
+	while True:
+		# If frontier is empty, there is no solution
+		if frontier == []:
+			print("IMPOSSIBLE")
+			return
+		else:
+			# Choose first element in Frontier and check if goal is reached
+			node = frontier.pop(0)
+			if is_goal(end, node):
+				return solved(node, direction)
+			else:
+				expand(node, frontier, seen)
+
+
+# END OF BFS PATH PLANNING
+# --------------------------------------------------------------------------------------------
+
+
+# START OF ROBOT MOTION
+# --------------------------------------------------------------------------------------------
+
+
+# Class for the self-driving robot
+class AutoDriver:
+
+    # Initializes the robot
+    def __init__(self, path):
         self.camera = cv2.VideoCapture(0)
         self.frame = None
 	self.bridge = CvBridge()
@@ -21,76 +236,66 @@ class ImageCapture:
 	self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.camera_callback)
 	self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
 
-
+	# Publisher and message to move robot
 	self.cmd_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=0)
+	self.mvmt_msg = Twist()
+
 	self.rate = rospy.Rate(100)
 
-	self.state = 0
+	self.state = 0 # Current state of the robot in FSM
 
+	# Gains for PD
+	self.kp = 0.5
+	self.kd = 3
 
-	self.kp = 1
-	self.kd = 5
-
+	# Error values for PD
 	self.delta_error = 0
 	self.error = 1
 
-	self.frame_number = 0
+	# Queue of directions for path following
+	self.directions = path
 
-	self.mvmt_msg = Twist()
-
-	self.directions = deque()
-	self.directions.append(0) # 0 = straight, 1 = left, -1 = right
-	self.directions.append(0)
-	self.directions.append(-1)
-
-	self.current_turn = 0
-
+	# HTML used to train robot for stop sign detection
 	self.stopSignCascade = cv2.CascadeClassifier("stop_sign.xml")
 
-	
+	# Flags for state determination
 	self.obstacle = False
 	self.at_stopsign = True
 	self.at_intxn = False
 	self.turning = False
+	self.current_turn = 0
 
 
-    def scale(self, percent):
-	scale_percent = percent # percent of original size
-        width = int(self.frame.shape[1] * scale_percent / 100)
-        height = int(self.frame.shape[0] * scale_percent / 100)
-        dim = (width, height)
-        self.frame = cv2.resize(self.frame, dim, interpolation = cv2.INTER_AREA)
-
+    # Callback for the LIDAR scanner
     def scan_callback(self, data):
+
+	# Use just the front 60 degrees of scan measurements
 	num_measurements = len(data.ranges)
 	midpoint = int(num_measurements/2)
 	front_scan_1 = data.ranges[0:30]
 	front_scan_2 = data.ranges[num_measurements-30:num_measurements]
-
 	front_scan = front_scan_1 + front_scan_2
 
+	# Checks if an obstacle is in front of the robot
 	for i in front_scan:
 		if i < 0.5 and not i == "inf":
 			self.obstacle = True
-			#self.state = 0
 			return
 
 	self.obstacle = False
 
-	#print self.at_intxn
-	#if not self.at_intxn and not self.turning and not self.visibleStopSigns:
-	#	self.state = 1
 
-
+    # Callback for the RGB camera
     def camera_callback(self, data):
 	try:
 		self.frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
 	except CvBridgeError as e:
 		print(e)
 
+	# Convert frame to gray
 	gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
-	# analyse stop signs
+	# Analyze stop signs
 	stopSigns = self.stopSignCascade.detectMultiScale(
 		gray,
 		scaleFactor=1.25,
@@ -99,6 +304,7 @@ class ImageCapture:
 		flags=cv2.CASCADE_SCALE_IMAGE
 	)
 	
+	# Checks if a stop sign is detected
 	if len(stopSigns) > 0:
 		# debugging only
 		"""for (x, y, w, h) in stopSigns:
@@ -110,19 +316,22 @@ class ImageCapture:
 	
 	self.at_stopsign = False
 	
+	# Crop the frame and filter into black and white for line detection
 	self.frame = self.frame[300:480, 0:640]
-	# process the image into lines and then detect main line)
 	lbound = np.array([0,0,0], dtype = "uint8")
 	ubound = np.array([50,50,50], dtype = "uint8")
 	mask = cv2.inRange(self.frame, lbound, ubound)
 	edges = cv2.Canny(mask, 50, 150, apertureSize = 3)
-	lines = cv2.HoughLines(edges, 1, np.pi/180, 75) # might look into detecting largest line
-		
+	lines = cv2.HoughLines(edges, 1, np.pi/180, 75) # Finds the lines
+
+	# Prevents line detection if currently turning
 	if self.turning:
 		return
 	
-	# finds the x-coord of the middle of the frame
+	# Finds the x-coord of the middle of the frame
 	middle_x = int(self.frame.shape[1]/2)
+
+	# If lines found, calculate error for PD
 	if lines is not None:
 		for rho, theta in lines[0]:
 			line = self.generateLine(rho, theta)
@@ -135,31 +344,31 @@ class ImageCapture:
 		self.delta_error = min(calculated_error - self.error, 100)
 		self.error = calculated_error
 
+		# Checks to see if robot is at an intersection by finding horizontal lines
 		for line in lines:
 			for rho, theta in line:
 				curr_line = self.generateLine(rho, theta)
 				x1,y1 = curr_line[0]
 				x2,y2 = curr_line[1]
-			if np.absolute(y1 - y2) < 150:
-				#self.state = 2
+			if np.absolute(y1 - y2) < 150: # Checks if the line is horizontal within a threshold
 				self.at_intxn = True
-				#print ("Found perp line")
 				return
-			#else:
-				#print("NOT FOUND")
+	
 		self.at_intxn = False
 
 	else:
+		# If no lines found, stop robot
 		self.state = 0
 
 
-	# setup the ground truth line
+	# Draws the ground truth line
 	cv2.line(self.frame, (middle_x, 0), (middle_x, self.frame.shape[0]), (255,255,255),2)
-	# for visualization and report information
 
-	# Display the resulting frame
+	# Display the resulting frame (Comment this out to make robot run faster)
 	#self.render(self.frame)
 
+
+    # Calculates the error for the PD
     def calc_error(self, x1, y1, x2, y2, middle_x):
 	if x2-x1 == 0.0:
 		return middle_x - x1
@@ -172,12 +381,16 @@ class ImageCapture:
 	else:
 		c_x1 = (0-b)/slope
 		c_x2 = (self.frame.shape[0]-b)/slope
+
+	# Finds average deviation
 	x_dev = 0
 	x_dev += middle_x - c_x1
 	x_dev += middle_x - c_x2
 
 	return x_dev/2.0
 
+
+    # PD line follower method
     def follow_line(self):
 	proportional_component = self.kp * self.error
 	derivative_component = self.kd * self.delta_error
@@ -187,22 +400,17 @@ class ImageCapture:
 	self.mvmt_msg.angular.z = error_correction
 	#self.renderText("Command: " + str(error_correction))
 
-	#max(min(error_correction, angular_cap), -1 * angular_cap)
-        #print("angular change z: %f" % msg.angular.z)
 
-	# self.cmd_pub.publish(self.mvmt_msg)
-
+    # Method to make left, right, and straight turns
     def make_turn(self):
 	if self.turning:
-		time = 4
-		if self.current_turn == 0:
-			time = 4.5
+		time = 4 # Time taken to turn
 		if rospy.get_time() - self.turn_start_time > time:
 			self.turning = False
 			self.at_intxn = False
 			#self.state = 1
 		else:
-			self.mvmt_msg.angular.z = self.current_turn * np.pi/8
+			self.mvmt_msg.angular.z = self.current_turn * np.pi/6
 			self.mvmt_msg.linear.x = 0.1
 	elif self.directions:
 		print("Popping")
@@ -253,18 +461,13 @@ class ImageCapture:
         y0 = b*rho
         x1 = int(x0 + 1000*(-b))
         y1 = int(y0 + 1000*(a))
-        x2 = int(x0 - 1000*(-b))
+        x2 = int(x0 - 1000*(-b))Make_map
         y2 = int(y0 - 1000*(a))
 
         return [(x1, y1), (x2, y2)]
 
     def render(self, image):
         cv2.imshow('frame', image)
-
-	cv2.imwrite("frames/frame_%d.jpg" % self.frame_number, image)
-
-	self.frame_number += 1
-
 	cv2.waitKey(1)
 
     def renderText(self, text, position=(10, 25), color=(0, 255, 0)):
@@ -275,6 +478,11 @@ class ImageCapture:
 
 if __name__ == '__main__':
 	rospy.init_node('image_capture')
-	capture = ImageCapture()
-	capture.start()
-	capture.stop()
+
+	# Making the map and finding path
+	node_list = make_map()
+	path = bfs(node_list[START], node_list[END], DIRECTION) # (Start node, End node, Direction)
+
+	robot = AutoDriver(path)
+	robot.start()
+	robot.stop()
